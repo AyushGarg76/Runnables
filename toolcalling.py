6d31a9b77767d2c1effa00e5cda10ed0cf74d44b
AyushGarg <ayushstar76@gmail.com>
2026-06-05 17:11:22 +0530

    Updated toolcalling file

--- toolcalling.py
+++ toolcalling.py
@@ -2,6 +2,7 @@ from dotenv import load_dotenv
 load_dotenv()
 from langchain_mistralai import ChatMistralAI
 from langchain.tools import tool 
+from langchain_core.messages import HumanMessage
 from rich import print
 
 #1 creating a tool
@@ -11,14 +12,28 @@ def get_text_length(text: str) -> int:
     """Returns the number of characters in a given text"""
     return len(text)
 
+tools = {
+    "get_text_length":get_text_length
+}
 
 llm = ChatMistralAI(model_name = "mistral-small-2506")
 
 #Tool Binding 
 llm_with_tool = llm.bind_tools([get_text_length])
 
-result = llm_with_tool.invoke("Return the number of character in a given text: 'Hello how are you?'")
+message = []
+prompt = input("You: ")
+query = HumanMessage(prompt)
+message.append(query)
 
-print(result.tool_calls)
+result = llm_with_tool.invoke(message)
 
-print(get_text_length.invoke({'text': 'hello how are you'}))
+message.append(result)
+
+if result.tool_calls:
+    tool_name = result.tool_calls[0]["name"]
+    tool_message = tools[tool_name].invoke(result.tool_calls[0])
+    message.append(tool_message)
+
+result = llm_with_tool.invoke(message)
+print(result.content)
